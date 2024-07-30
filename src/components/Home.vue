e94696f10ca220fe61b435e2d871a5c208c69215
QingjieShen <122641381+QingjieShen@users.noreply.github.com>
2024-07-29 21:40:06 -0400

    router setup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <router-view></router-view>
+  <h1>Hello!</h1>
 </template>
 
 <style scoped>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../components/Home.vue';
+import Detail from '../components/Detail.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/edit/:id',
+    name: 'Detail',
+    component: Detail,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
