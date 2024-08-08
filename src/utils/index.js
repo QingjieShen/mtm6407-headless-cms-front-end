07722cd2948154e4571cd91e0470257c10ad8b1b
QingjieShen <122641381+QingjieShen@users.noreply.github.com>
2024-08-08 18:21:29 -0400

    adds single vehicle data fetch link

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
 export const ALL_PRODUCTS_URL = 'https://mtm6407-headless-cms-back-end.onrender.com/api/vehicles?populate=*';
+export const ONE_PRODUCTS_URL = 'https://mtm6407-headless-cms-back-end.onrender.com/api/vehicles';
